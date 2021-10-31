e498d8332d187c0debf9c3e135ae9ab89bc669ee
yashpreetbathla <32976561+yashpreetbathla@users.noreply.github.com>
2021-10-31 12:41:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+def gen1(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
